076cfecf0333eece904a21386548685c9090f48f
fedusiv <fedusivilya@gmail.com>
2021-05-29 13:41:48 +0300

    Removed world situation data module

--- server/news/world_situation_data.py
+++
@@ -1,9 +0,0 @@
-from news.news_types import InfluenceStage
-# Inforamtion element with world situation
-class WorldSituationData():
-    def __init__(self):
-        self.war = InfluenceStage.NONE
-        self.scince = InfluenceStage.NONE
-        self.entertainment = InfluenceStage.NONE
-        self.hardware = InfluenceStage.NONE
-        self.social = InfluenceStage.NONE
